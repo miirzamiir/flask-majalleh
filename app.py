6a2c2ae98c34acec61d4c4ecd8390310e5ba668f
stupidprogrammer4 <captainjoker757@gmail.com>
2024-08-27 21:48:28 +0330

    add 404 page and create article pages

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from secrets import token_hex
 from dotenv import load_dotenv
 from flask_migrate import Migrate
 import os
-from werkzeug.security import generate_password_hash, check_password_hash
 from article.models import Article, Category, Bookmark, Vote, Comment
 from user.models import User, Profile
+from article.routes import article_bp
+from user.routes import user_bp
 
 
 load_dotenv()
@@ -23,78 +24,22 @@ app.secret_key = token_hex(16)
 db.init_app(app)
 migrate = Migrate(app, db)
 
-# app.register_blueprint(user_bp, url_prefix='/user')
-# app.register_blueprint(article_bp, url_prefix='/article')
+app.register_blueprint(article_bp)
+app.register_blueprint(user_bp, url_prefix='/users')
 
 @app.get('/')
+@app.get('/index')
 def home():
-
-    context = {
-        'articles': Article.query.order_by(Article.date.desc()).limit(10).all(),
-        'categories': Category.query.order_by(Category.label).all(),
-        'tag_box': Article.tag_box_selector(session=db.session),
-        'slider': Article.slider(session=db.session)
-    }
-    return render_template('pages/index.html', context=context)
+    articles =  Article.query.order_by(Article.date.desc()).limit(10).all()
+    categories =  Category.query.order_by(Category.label).all()
+    tag_box =  Article.tag_box_selector(session=db.session)
+    slider =  Article.slider(session=db.session)
+    return render_template('pages/index.html', articles=articles, categories=categories, tag_box=tag_box, slider=slider)
 
 @app.get('/about')
 def about():
-    context = {
-        'categories': Category.query.order_by(Category.label).all()
-    }
-    return render_template('pages/about.html', context=context)
-
-@app.get('/login')
-def login():
-    return render_template('account/login.html')
-
-@app.post('/loginpost')
-def login_post():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
-    redirect_path = '/login'
-
-    user = User.query.filter(User.username==username).first()
-    if user and check_password_hash(user.password, password):
-        session['username'] = user.username
-        redirect_path = '/'
-    else:
-        flash('ورود ناموفق. اطلاعات ورودی را بازبینی کنید.', 'danger')
-    
-    return redirect(redirect_path)
-
-@app.get('/register')
-def register():
-    return render_template('account/register.html')
-
-@app.post('/registerpost')
-def register_post():
-    username = request.form.get('username')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    re_password = request.form.get('repassword')
-
-    redirect_path = url_for('register')
-    existing_user = User.query.filter((User.username==username) | (User.email==email)).first()
-    
-    if existing_user:
-        flash('نام کاربری یا ایمیل تکراری است.', 'danger')
-
-    elif password!=re_password:
-        flash('رمز عبور با تکرارش مطابقت ندارد.', 'danger')
-
-
-    elif password==re_password:
-        hashed_password = generate_password_hash(password=password)
-        new_user = User(username=username, email=email, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        
-        flash('ثبت نام با موفقیت انجام شد.', 'success')
-        redirect_path = '/login'
-
-    return redirect(redirect_path)
+    categories =  Category.query.order_by(Category.label).all()
+    return render_template('pages/about.html', categories=categories)
 
 @app.get('/search')
 def search():
@@ -106,7 +51,13 @@ def logout():
     flash('You have been logged out.', 'success')
     return redirect('/')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    categories =  Category.query.order_by(Category.label).all()
+    return render_template('errors/notfound.html', categories=categories), 404
+
+
 
 if __name__ == '__main__':
-    app.run(host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT, debug=True)
     
--- user/routes.py
+++ user/routes.py
@@ -1,9 +1,62 @@
-from flask import Blueprint, render_template
-from .models import ArticleModel  # Import your model here
+from flask import Blueprint, render_template, redirect, session, request, flash, url_for
+from .models import *
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
 
-article_bp = Blueprint('article', __name__)
+user_bp = Blueprint('user', __name__)
 
-@article_bp.route('/articles')
-def list_articles():
-    articles = ArticleModel.query.all()
-    return render_template('articles.html', articles=articles)
+
+@user_bp.get('/login')
+def login():
+    resp = redirect('pages/index')
+    if 'username' not in session:
+        resp = render_template('account/login.html')
+    return resp
+
+@user_bp.post('/loginpost')
+def login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    redirect_path = '/users/login'
+
+    user = User.query.filter(User.username==username).first()
+    if user and check_password_hash(user.password, password):
+        session['username'] = user.username
+        redirect_path = '/'
+    else:
+        flash('ورود ناموفق. اطلاعات ورودی را بازبینی کنید.', 'danger')
+    
+    return redirect(redirect_path)
+
+@user_bp.get('/register')
+def register():
+    return render_template('account/register.html')
+
+@user_bp.post('/registerpost')
+def register_post():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    re_password = request.form.get('repassword')
+
+    redirect_path = url_for('user.register')
+    existing_user = User.query.filter((User.username==username) | (User.email==email)).first()
+    
+    if existing_user:
+        flash('نام کاربری یا ایمیل تکراری است.', 'danger')
+
+    elif password!=re_password:
+        flash('رمز عبور با تکرارش مطابقت ندارد.', 'danger')
+
+
+    elif password==re_password:
+        hashed_password = generate_password_hash(password=password)
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('ثبت نام با موفقیت انجام شد.', 'success')
+        redirect_path = '/users/login'
+
+    return redirect(redirect_path)
